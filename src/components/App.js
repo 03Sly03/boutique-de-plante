30e4d0aa8226d7c4d2aa082d3eb009dd305c46e1
sly <slydevwebproject@outlook.fr>
2023-02-26 15:44:32 +0100

    update; modifcation of the db location; (images stored in src/assets folder)

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,30 @@ import Cart from './Cart';
 import Footer from './Footer';
 import QuestionForm from './QuestionForm';
 import ShoppingList from './ShoppingList';
-import axios from 'axios';
+import { dbPlantList } from '../datas/plantList';
+// import axios from 'axios';
 
 function App() {
   const retrievedItem = window.localStorage.getItem('MyCart');
-  const [cart, updateCart] = useState(retrievedItem ? JSON.parse(retrievedItem) : []);
+  const [cart, updateCart] = useState(
+    retrievedItem ? JSON.parse(retrievedItem) : []
+  );
   const [plants, setPlants] = useState([]);
   const [isFooterShown, setIsFooterShown] = useState(true);
   const [catFilter, setCatFilter] = useState([]);
-  const [plantList, setPlantList] = useState([])
+  const [plantList, setPlantList] = useState([]);
 
   const categories = plantList.reduce(
-    (acc, plant) => acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
     []
-);
+  );
 
   function sortByCategory(category) {
     if (category) {
       const filterByCategory = plantList.filter(
         (plant) => plant.category === category
-      )
+      );
       setPlants(filterByCategory);
     } else {
       setPlants(plantList);
@@ -37,12 +41,19 @@ function App() {
     if (currentPlantAdded) {
       const cartFilteredCurrentPlant = cart.filter(
         (plant) => plant.name !== name
-      )
+      );
       updateCart(
-        [...cartFilteredCurrentPlant, { name, price, amount: currentPlantAdded.amount + 1 }].sort((a, b) => a.name > b.name ? 1 : -1)
-      )
+        [
+          ...cartFilteredCurrentPlant,
+          { name, price, amount: currentPlantAdded.amount + 1 },
+        ].sort((a, b) => (a.name > b.name ? 1 : -1))
+      );
     } else {
-      updateCart([...cart, { name, price, amount: 1 }].sort((a, b) => a.name > b.name ? 1 : -1));
+      updateCart(
+        [...cart, { name, price, amount: 1 }].sort((a, b) =>
+          a.name > b.name ? 1 : -1
+        )
+      );
     }
   }
 
@@ -50,42 +61,60 @@ function App() {
     if (checked) {
       const filterByCategory = plantList.filter(
         (plant) => plant.category === category
-      )
+      );
       setCatFilter(catFilter.concat(filterByCategory));
-      setPlants(catFilter.concat(filterByCategory).sort((a, b) => a.name > b.name ? 1 : -1));
+      setPlants(
+        catFilter
+          .concat(filterByCategory)
+          .sort((a, b) => (a.name > b.name ? 1 : -1))
+      );
     } else if (!checked && catFilter[0]) {
-      const element = catFilter.filter(el => el.category !== category);
+      const element = catFilter.filter((el) => el.category !== category);
       setCatFilter(element);
-      element[0] ? setPlants(element.sort((a, b) => a.name > b.name ? 1 : -1)) : setPlants(plantList.sort((a, b) => a.name > b.name ? 1 : -1));
+      element[0]
+        ? setPlants(element.sort((a, b) => (a.name > b.name ? 1 : -1)))
+        : setPlants(plantList.sort((a, b) => (a.name > b.name ? 1 : -1)));
     } else {
       // in case of bug: checked equal false and no catFilter array
-      setPlants(plantList.sort((a, b) => a.name > b.name ? 1 : -1));
+      setPlants(plantList.sort((a, b) => (a.name > b.name ? 1 : -1)));
     }
   }
 
   useEffect(() => {
-    axios.get('http://localhost:3001/plantList')
-      .then(function (response) {
-        console.log(response.data);
-        setPlantList(response.data.sort((a, b) => a.name > b.name ? 1 : -1));
-        setPlants(response.data.sort((a, b) => a.name > b.name ? 1 : -1));
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }, [])
+    //   axios
+    //     .get('http://localhost:3001/plantList')
+    //     .then(function (response) {
+    //       console.log(response.data);
+    //       setPlantList(response.data.sort((a, b) => (a.name > b.name ? 1 : -1)));
+    //       setPlants(response.data.sort((a, b) => (a.name > b.name ? 1 : -1)));
+    //     })
+    //     .catch(function (error) {
+    //       console.log(error);
+    //     });
+    setPlantList(dbPlantList.sort((a, b) => (a.name > b.name ? 1 : -1)));
+    setPlants(dbPlantList.sort((a, b) => (a.name > b.name ? 1 : -1)));
+  }, []);
 
   return (
     <div>
       <Banner />
-      <div className='shop'>
+      <div className="shop">
         <Cart cart={cart} updateCart={updateCart} addToCart={addToCart} />
-        <ShoppingList cart={cart} updateCart={updateCart} addToCart={addToCart} sortByCategory={sortByCategory} plants={plants} addCategoryToShop={addCategoryToShop} categories={categories} />
+        <ShoppingList
+          cart={cart}
+          updateCart={updateCart}
+          addToCart={addToCart}
+          sortByCategory={sortByCategory}
+          plants={plants}
+          addCategoryToShop={addCategoryToShop}
+          categories={categories}
+        />
       </div>
       <QuestionForm />
       <button onClick={() => setIsFooterShown(false)}>Cacher le footer</button>
       {isFooterShown && <Footer cart={cart} />}
-    </div>);
+    </div>
+  );
 }
 
 export default App;
